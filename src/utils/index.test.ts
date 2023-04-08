b348d688c4b07afa395a8f59e949be0c333940a0
pantinho <pantinho>
2023-04-08 09:03:30 +0700

    update connect wallet modal

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -3,7 +3,7 @@ import { describe, it, expect } from 'vitest'
 
 describe('shorten address', () => {
     const testAddress = '0x53Dc36E8E23c1C726c7b4f50CF95034D9b9932Dc'
-    it('address must be shorter', () => {
+    it('address should be shorter than before', () => {
         const sA = shortenAddress(testAddress)
         expect(shortenAddress(testAddress).length).toBeLessThan(
             testAddress.length,
