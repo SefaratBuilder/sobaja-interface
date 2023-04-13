c12f846919e79223212c0066aa2da66a07361996
matthew <matthewsleepatcode@gmail.com>
2023-04-13 17:56:22 +0700

    update modal transaction

---
+++ src/components/TransactionModal/ResultTransaction/index.tsx
@@ -0,0 +1,124 @@
+import PrimaryButton from 'components/Buttons/PrimaryButton'
+import styled from 'styled-components'
+import imgClose from 'assets/icons/x.svg'
+import imgSuccess from 'assets/icons/success.svg'
+import imgError from 'assets/icons/error.svg'
+
+import { useAppState } from 'states/application/hooks'
+import { useOnClickOutside } from 'hooks/useOnClickOutSide'
+import { useRef } from 'react'
+
+interface ResultModal {
+    isSuccess: boolean
+    setOpenModal: any
+    txnHash: string | undefined
+    error?: any
+}
+const ResultTransactionModal = ({
+    isSuccess,
+    setOpenModal,
+    txnHash,
+    error,
+}: ResultModal) => {
+    const { userDarkMode } = useAppState()
+    const ref = useRef()
+
+    useOnClickOutside(ref, () => {
+        setOpenModal(false)
+    })
+
+    return (
+        <Container
+        // ref={ref}
+        >
+            <Header>
+                <ImgClose
+                    src={imgClose}
+                    alt=""
+                    onClick={() => setOpenModal(false)}
+                />
+            </Header>
+            <WrapContent>
+                <WrapImgResult>
+                    <img src={`${isSuccess ? imgSuccess : imgError}`} alt="" />
+                </WrapImgResult>
+                <WrapInfoLoad>
+                    <div>
+                        {isSuccess
+                            ? 'Transaction Submitted'
+                            : error?.data
+                            ? error?.data
+                            : error}
+                    </div>
+                </WrapInfoLoad>
+            </WrapContent>
+            <PrimaryButton
+                type="light-blue"
+                name="Close"
+                onClick={() => setOpenModal(false)}
+            />
+        </Container>
+    )
+}
+
+export default ResultTransactionModal
+
+const Container = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 3;
+    border: 1.5px solid var(--border2);
+    border-radius: 12px;
+    padding: 20px 25px;
+    background: linear-gradient(
+        to top right,
+        rgba(0, 28, 44, 0.3),
+        rgba(0, 28, 44, 0.3)
+    );
+    box-shadow: rgb(0 0 0 / 5%) 0px 4px 8px 0px;
+    max-width: 500px;
+    width: 100%;
+    padding: 20px 30px;
+    backdrop-filter: blur(40px);
+`
+const Header = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+const WrapContent = styled.div`
+    margin-bottom: 30px;
+`
+const WrapImgResult = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 30px 0;
+    img {
+        height: 80px;
+        width: 80px;
+    }
+`
+const WrapInfoLoad = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    div:first-child {
+        font-size: 20px;
+    }
+    div:last-child {
+        color: #ffffffba;
+        font-size: 14px;
+    }
+    a {
+        text-decoration: none;
+        color: rgb(1, 104, 255);
+    }
+`
+const ImgClose = styled.img`
+    width: 20px;
+    height: 20px;
+`
---
+++ src/components/TransactionModal/WaitingTransaction/index.tsx
@@ -0,0 +1,166 @@
+import Blur from 'components/Blur'
+// import HiddenGlobalScroll from "components/HiddenGlobalScroll"
+// import { Loader } from "components/Loader"
+// import { payloadTxnModal } from "constants/index"
+import { useOnClickOutside } from 'hooks/useOnClickOutSide'
+import React, { useEffect, useRef, useState } from 'react'
+import styled from 'styled-components'
+import imgClose from 'assets/icons/x.svg'
+import Loader from 'components/Loader'
+
+interface WaitingTransactionModalProps {
+    setModalRemove?: any
+    payloadTxn?: any
+}
+
+const WaitingTransactionModal = ({
+    setModalRemove,
+    payloadTxn,
+}: WaitingTransactionModalProps) => {
+    const [message, setMessage] = useState<string>('')
+    const ref = useRef()
+
+    useEffect(() => {
+        if (payloadTxn && payloadTxn?.method) {
+            // const method = payloadTxn?.method
+            switch (payloadTxn.method) {
+                case 'Add':
+                    setMessage(
+                        `Supplying ${
+                            payloadTxn.amountInNoDecimals +
+                            ' ' +
+                            payloadTxn.tokenIn
+                        } and ${
+                            payloadTxn.amountOutNoDecimals +
+                            ' ' +
+                            payloadTxn.tokenOut
+                        }`,
+                    )
+                    break
+                case 'Swap':
+                    setMessage(
+                        `Swap ${
+                            payloadTxn?.amountIn + ' ' + payloadTxn.tokenIn
+                        } for ${
+                            payloadTxn?.amountOut + ' ' + payloadTxn.tokenOut
+                        }`,
+                    )
+                    break
+                case 'Remove':
+                    {
+                        setMessage(
+                            `Remove ${
+                                payloadTxn.amountIn + ' ' + payloadTxn.tokenIn
+                            } for ${
+                                payloadTxn.amountOut.coinX +
+                                ' ' +
+                                payloadTxn.tokenOut.coinX
+                            } and ${
+                                payloadTxn.amountOut.coinY +
+                                ' ' +
+                                payloadTxn.tokenOut.coinY
+                            }`,
+                        )
+                    }
+                    break
+                case 'Faucet':
+                    {
+                        setMessage('')
+                    }
+                    break
+                default:
+                    setMessage('')
+                    break
+            }
+        }
+    }, [payloadTxn])
+
+    useOnClickOutside(ref, () => {
+        setModalRemove(false)
+    })
+
+    return (
+        <>
+            {/* <HiddenGlobalScroll /> */}
+            <Container>
+                <Header>
+                    <ImgClose
+                        onClick={() => setModalRemove(false)}
+                        src={imgClose}
+                        alt=""
+                    />
+                </Header>
+                <WrapContent>
+                    <WrapInfoLoad>
+                        <div>Waiting For Confirmation</div>
+                        <WrapLoader>
+                            <Loader size="42px" />
+                        </WrapLoader>
+                        <div>{message}</div>
+                        <div>Confirm this transaction in your wallet</div>
+                    </WrapInfoLoad>
+                </WrapContent>
+            </Container>
+            <Blur />
+        </>
+    )
+}
+
+export default WaitingTransactionModal
+const Container = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 3;
+    border: 1.5px solid var(--border2);
+    border-radius: 12px;
+    padding: 20px 25px;
+    background: linear-gradient(
+        to top right,
+        rgba(0, 28, 44, 0.3),
+        rgba(0, 28, 44, 0.3)
+    );
+    box-shadow: rgb(0 0 0 / 5%) 0px 4px 8px 0px;
+    max-width: 500px;
+    width: 100%;
+    padding: 20px 30px;
+    backdrop-filter: blur(40px);
+`
+const Header = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+const WrapContent = styled.div``
+const WrapLoader = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 25px 0;
+`
+const WrapInfoLoad = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    div {
+        text-align: center;
+    }
+
+    div:first-child {
+        font-size: 24px;
+    }
+    div:last-child {
+        color: #ffffffba;
+        font-size: 14px;
+    }
+    @media screen and (max-width: 390px) {
+        text-align: center;
+    }
+`
+const ImgClose = styled.img`
+    width: 16px;
+    /* height: 20px; */
+    cursor: pointer;
+`
