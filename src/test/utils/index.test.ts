b1046819385eb14fe9bdbe97c64eacd362b13a84
spaceNhat <space.nhoanganhtuan@gmail.com>
2023-04-14 10:48:31 +0700

    calcSlippageAmount feature

--- src/test/utils/index.test.ts
+++ src/test/utils/index.test.ts
@@ -1,12 +1,28 @@
-import { shortenAddress } from 'utils'
+import {  shortenAddress } from 'utils'
 import { describe, it, expect } from 'vitest'
+import { calcSlippageAmount } from 'utils'
 
-describe('shorten address', () => {
-    const testAddress = '0x53Dc36E8E23c1C726c7b4f50CF95034D9b9932Dc'
-    it('address should be shorter than before', () => {
-        const sA = shortenAddress(testAddress)
-        expect(shortenAddress(testAddress).length).toBeLessThan(
-            testAddress.length,
-        )
+// describe('shorten address', () => {
+//     const testAddress = '0x53Dc36E8E23c1C726c7b4f50CF95034D9b9932Dc'
+//     it('address should be shorter than before', () => {
+//         const sA = shortenAddress(testAddress)
+//         expect(shortenAddress(testAddress).length).toBeLessThan(
+//             testAddress.length,
+//         )
+//     })
+// })
+describe('calculateSlippageAmount',()=>{
+    it('price is correct',() =>{
+        const amountOutMin = calcSlippageAmount("10000","0.5")[0]
+        const amountInMax = calcSlippageAmount("10000","0.5")[1]
+
+        const test = calcSlippageAmount("10000","0.5")
+        console.log(test);
+
+        expect(amountOutMin).toEqual('5000')
+        expect(amountInMax).toEqual('20000')
     })
 })
+
+
+
