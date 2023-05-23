e01f12c5ab76ad9ee2553ff4ea5e5d53dabb952d
matthew <matthewsleepatcode@gmail.com>
2023-05-23 18:02:01 +0700

    update ui faucet pending

--- src/components/Faucet/index.tsx
+++ src/components/Faucet/index.tsx
@@ -21,14 +21,30 @@ import { useTransactionHandler } from 'states/transactions/hooks'
 import axios from 'axios'
 import { FaucetTokens } from 'constants/addresses'
 import { ChainId } from 'interfaces'
+import BGSoba from 'assets/icons/soba2.jpg'
+import Twitter from 'assets/icons/twitter.svg'
+import Discord from 'assets/icons/discord.svg'
+import CloseButton from 'assets/icons/icon-close.svg'
 
 const Faucet = () => {
     const [isDislayFaucet, setIsDisplayFaucet] = useState<boolean>(false)
+    const [isFaucetETH, setIsFaucetETH] = useState<boolean>(false)
+    // const [indexFaucetETH, setIndexFaucetETH] = useState(0)
     const ref = useRef<any>()
     const faucetContract = useFaucetContract()
     const { account, chainId, provider } = useActiveWeb3React()
     const initDataTransaction = InitCompTransaction()
     const { addTxn } = useTransactionHandler()
+
+    const [currentStep, setCurrentStep] = useState(2)
+    const listFaucetETH = [
+        { name: 'Goerli', chainId: ChainId.GOERLI },
+        { name: 'zkSync Era', chainId: ChainId.ZKTESTNET },
+    ]
+    const listSocials = [
+        { name: 'Twitter', details: 'Follow & Tweet', img: Twitter },
+        { name: 'Discord', details: 'Join Discord', img: Discord },
+    ]
 
     const isDisable = useMemo(
         () =>
@@ -138,27 +154,14 @@ const Faucet = () => {
 
     const showMintCoins = () => {
         if (chainId && FaucetTokens?.[chainId].length > 0) {
-            const sortList = FaucetTokens?.[chainId]
-            // .sort((a, b) => {
-            //     const symbolA = a.symbol.toUpperCase() // ignore upper and lowercase
-            //     const symbolB = b.symbol.toUpperCase() // ignore upper and lowercase
-            //     if (symbolA < symbolB) {
-            //         return -1
-            //     }
-            //     if (symbolA > symbolB) {
-            //         return 1
-            //     }
-
-            //     return 0
-            // })
-            return sortList.map((item, index) => {
+            return FaucetTokens?.[chainId].map((item, index) => {
                 return (
                     <PrimaryButton
                         key={index}
                         name={item.symbol}
                         onClick={() =>
                             item.address === ZERO_ADDRESS
-                                ? handleFaucetETH()
+                                ? setIsFaucetETH(true)
                                 : clickFaucetToken(item.address)
                         }
                         disabled={isDisable}
@@ -168,51 +171,6 @@ const Faucet = () => {
                 )
             })
         }
-
-        // if (tokenList && tokenList.length > 0) {
-        //     const tokens =
-        //         chainId === 280
-        //             ? [
-        //                   {
-        //                       address: ZERO_ADDRESS,
-        //                       symbol: 'ETH',
-        //                       type: 'faucet',
-        //                       logoURI: ETH,
-        //                       chainId: 280,
-        //                   },
-        //                   ...tokenList.sort((a, b) => {
-        //                       const symbolA = a.symbol.toUpperCase() // ignore upper and lowercase
-        //                       const symbolB = b.symbol.toUpperCase() // ignore upper and lowercase
-        //                       if (symbolA < symbolB) {
-        //                           return -1
-        //                       }
-        //                       if (symbolA > symbolB) {
-        //                           return 1
-        //                       }
-
-        //                       return 0
-        //                   }),
-        //               ]
-        //             : tokenList
-        //     return tokens.map((item, index) => {
-        //         if (item.type == 'faucet' && item.chainId == 280) {
-        //             return (
-        //                 <PrimaryButton
-        //                     key={index}
-        //                     name={item.symbol}
-        //                     onClick={() =>
-        //                         item.address === ZERO_ADDRESS
-        //                             ? handleFaucetETH()
-        //                             : clickFaucetToken(item.address)
-        //                     }
-        //                     disabled={isDisable}
-        //                     img={item.symbol == 'USDT' ? usdt : item.logoURI}
-        //                     type={'faucet'}
-        //                 />
-        //             )
-        //         }
-        //     })
-        // }
     }
 
     return (
@@ -265,10 +223,205 @@ const Faucet = () => {
             ) : (
                 ''
             )}
-            {isDislayFaucet ? <Blur /> : ''}
+            {isDislayFaucet || isFaucetETH ? <Blur /> : ''}
+
+            {isFaucetETH && (
+                <DetailFaucetETH image={BGSoba}>
+                    <div
+                        className="close-button"
+                        onClick={() => setIsFaucetETH(false)}
+                    >
+                        <img src={CloseButton} alt="close-x" />
+                    </div>
+                    <SelectNetwork>
+                        {listFaucetETH.map((list, index) => {
+                            return (
+                                <div
+                                    className={`${
+                                        chainId === list.chainId
+                                            ? 'active cursor'
+                                            : 'cursor'
+                                    }`}
+                                    // onClick={() => setIndexFaucetETH(index)}
+                                >
+                                    <p>{list.name}</p>
+                                </div>
+                            )
+                        })}
+                    </SelectNetwork>
+                    <p className="details">
+                        Fast and reliable. 0.05 Goerli ETH/day.
+                    </p>
+                    <SelectSocials>
+                        {listSocials.map((social, index) => {
+                            return (
+                                <span
+                                    className={
+                                        index === currentStep
+                                            ? 'active-step'
+                                            : ''
+                                    }
+                                >
+                                    <div className="cursor">
+                                        <img src={social.img} alt="" />
+                                    </div>
+                                    <p className="cursor">{social.details}</p>
+                                </span>
+                            )
+                        })}
+                    </SelectSocials>
+                    <LabelAddress>
+                        <span>
+                            <p>Get ETH</p>
+                        </span>
+                        <span>{account}</span>
+                    </LabelAddress>
+                    <LabelButton>
+                        <PrimaryButton
+                            name="Faucet"
+                            onClick={() => handleFaucetETH()}
+                        />
+                    </LabelButton>
+                </DetailFaucetETH>
+            )}
         </>
     )
 }
+
+const LabelButton = styled.div`
+    width: 35%;
+    margin: 0 auto;
+`
+
+const LabelAddress = styled.div`
+    width: 90%;
+    display: flex;
+    background: rgba(0, 178, 255, 0.3);
+    border-radius: 6px;
+    margin: 35px auto;
+
+    span {
+        padding: 11px;
+        overflow: hidden;
+    }
+    span:nth-child(1) {
+        background: #00b2ff;
+        border-radius: 6px 0px 0px 6px;
+        padding: 11px 16px;
+        min-width: 96px;
+    }
+`
+
+const SelectSocials = styled.div`
+    display: flex;
+    width: 70%;
+    margin: 0 auto;
+    gap: 10px;
+    text-align: center;
+
+    span {
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    div {
+        width: 50px;
+        height: 50px;
+    }
+    img {
+        width: 100%;
+        height: 100%;
+    }
+    .active-step {
+        border: 1px solid #22c2a4;
+        padding: 5px;
+    }
+`
+
+const SelectNetwork = styled.div`
+    display: flex;
+    background: rgba(0, 178, 255, 0.3);
+    border-radius: 12px;
+    width: 50%;
+    margin: 0 auto;
+    /* justify-content: space-between; */
+    /* gap: 10px; */
+    p {
+        font-size: 20px;
+        font-weight: 600;
+        /* margin: auto; */
+        text-align: center;
+    }
+    div {
+        min-width: 50%;
+        padding: 10px 0;
+        /* padding: 10px 20px; */
+    }
+
+    .active {
+        background: #00b2ff;
+        border-radius: 12px;
+    }
+
+    @media screen and (max-width: 572px) {
+        p {
+            font-size: 16px;
+        }
+    }
+`
+
+const DetailFaucetETH = styled.div<{ image: string }>`
+    position: fixed;
+    /* padding: 38px 0; */
+
+    /* height: 100%; */
+    /* width: 100%; */
+    width: 100%;
+    max-width: 600px;
+    height: 100%;
+    max-height: 420px;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    z-index: 999;
+
+    border: 1px solid rgba(0, 59, 92, 1);
+    background: url(${({ image }) => image});
+    background-size: 750px;
+    background-repeat: no-repeat;
+    background-position: top;
+    border-radius: 12px;
+
+    .close-button {
+        text-align: end;
+        padding: 10px 20px 0;
+
+        img {
+            width: 20px;
+            height: 20px;
+            cursor: pointer;
+        }
+    }
+    .details {
+        padding: 20px 0 45px;
+        text-align: center;
+    }
+
+    .cursor {
+        cursor: pointer;
+    }
+
+    @media screen and (max-width: 660px) {
+        width: 90% !important;
+    }
+    @media screen and (max-width: 572px) {
+        font-size: 14px;
+    }
+`
 
 const Icon = styled.img`
     height: 25px;
