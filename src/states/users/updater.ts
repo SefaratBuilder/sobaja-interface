91dfd5762e039777efb82061747d56002dbb0992
matthew <matthewsleepatcode@gmail.com>
2023-06-03 12:24:32 +0700

    fix add token smart account

--- src/states/users/updater.ts
+++ src/states/users/updater.ts
@@ -2,23 +2,26 @@ import { useActiveWeb3React } from 'hooks';
 import { useEffect } from "react"
 import { useAddUser, useUsersState } from "./hooks"
 import { initBalanceToken } from './reducer';
+import { useSmartAccountContext } from 'contexts/SmartAccountContext';
 
 export default function Updater() {
     const addUser = useAddUser()
-    const { chainId } = useActiveWeb3React()
+    const { chainId, account } = useActiveWeb3React()
     const userData = useUsersState()
+    const { smartAccountAddress } = useSmartAccountContext()
 
     /**
      * @init init data balance when not exist
      */
     useEffect(() => {
         if (!chainId) return
+
         if (!userData) {
             addUser({
                 balances: [{ ...initBalanceToken(chainId) }],
                 activity: [],
             })
         }
-    }, [chainId])
+    }, [chainId, smartAccountAddress, account])
     return null
 }
